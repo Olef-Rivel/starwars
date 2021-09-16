ff6bfba4deb781c365fe6f0f267dc4a3c198f0c1
Olef-Rivel <90057465+Olef-Rivel@users.noreply.github.com>
2021-09-17 01:07:19 +0300

    Add files via upload

---
+++ лр 9 осмп.cpp
@@ -0,0 +1,253 @@
+﻿// лр 9 осмп.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "лр 9 осмп.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK Dialog(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY9, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY9));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY9));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MY9);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr); //создает перекрывающее, выскакивающее или дочернее окно
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) //создаём кнопку в оконной процедуре WndProc
+{
+    switch (message)
+    {
+    case WM_CREATE:
+    {
+        HWND button1 = CreateWindowEx( //CreateWindowEx создает перекрывающее, выпрыгивающее или дочернее окно с расширенным стилем
+            NULL,
+            TEXT("Button"),
+            TEXT("Dialog"),
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            100,
+            100,
+            100,
+            25,
+            hWnd,
+            HMENU(IDC_MYBUTTON1),
+            hInst,
+            NULL
+        );
+        ShowWindow(button1, 1);
+
+        HWND button2 = CreateWindowEx(
+            NULL,
+            TEXT("Button"),
+            TEXT("Exit"),
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            220,
+            100,
+            100,
+            25,
+            hWnd,
+            HMENU(IDC_MYBUTTON2),
+            hInst,
+            NULL
+        );
+        ShowWindow(button2, 1);
+    }
+    break;
+
+    case WM_COMMAND: //обработчик нажатия кнопки
+    {
+        //LOWORD содержит идентификатор управления кнопки. HIWORD указывает код уведомления.
+        int wmId = LOWORD(wParam);
+        int wmEvent = HIWORD(wParam);
+        // Разобрать выбор в меню:
+        switch (wmId)
+        {
+        case IDC_MYBUTTON1:
+            //BN_CLICKED посылается, когда пользователь нажимает кнопку
+            if (wmEvent == BN_CLICKED) {
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_MY9_DIALOG), hWnd, (DLGPROC)Dialog); 
+            }
+            break;
+        case IDC_MYBUTTON2:
+            if (wmEvent == BN_CLICKED) {
+                DestroyWindow(hWnd); //DestroyWindow уничтожает указанное окно
+            }
+            break;
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About); //Макрокоманда DialogBox создает модальное диалоговое 
+                                                                         //окно из ресурса шаблона. Cодержит диалоговое окно About
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd); //DestroyWindow уничтожает указанное окно
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY: //Сообщение WM_DESTROY отправляется тогда, когда окно разрушается
+                    //Оно отправляется оконной процедуре разрушаемого
+                   //окна после того, как окно удаляется с экрана
+        PostQuitMessage(0); //Функция PostQuitMessage указывает системе, что поток сделал запрос на то,
+                            //чтобы завершить свою работу (выйти)
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+//Процедура диалогового окна
+LRESULT CALLBACK Dialog(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_INITDIALOG:
+        return true;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK) {
+            MessageBox(NULL, TEXT("How do  you?"), TEXT("Bye"), MB_OK);
+        }
+        else {
+            EndDialog(hDlg, LOWORD(wParam));
+        }
+        
+        return TRUE;
+        break;
+    }
+    return FALSE;
+}
+
